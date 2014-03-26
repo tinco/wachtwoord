811f9d97c04a11b12568519906bb650827639c2d
Tinco Andringa <mail@tinco.nl>
2014-03-26 15:00:01 +0100

    use cryptographic secure random generator

--- wachtwoord.rb
+++ wachtwoord.rb
@@ -1,3 +1,11 @@
+require 'securerandom'
+
+class Rng
+	def self.rand(lim)
+		SecureRandom.random_number(lim)
+	end
+end
+
 suitable = File.readlines('nl.dic')
 	.map(&:chomp)
 	.select{|w|w.length >= 3}
@@ -8,8 +16,9 @@ keyspace = suitable.length ** amount
 guesses_per_second = 1_000_000
 seconds_in_year = 365 * 24 * 60 * 60
 years_required = keyspace  / guesses_per_second / seconds_in_year
+password = amount.times.map{suitable.sample(random: Rng)}.join('')
 
 puts "Suitable passwords: #{suitable.length}"
 puts "Entropy for #{amount} words: 2^#{Math.log2(keyspace)}"
 puts "Years required at 1 million guesses/second: #{years_required.round}"
-puts "Password: #{amount.times.map{suitable.sample}.join('')}"
+puts "Password: #{password}"
